2dab2a1580d85a4ef0a2faa5fb3daf0a97d02c62
Vaibhavbairagi <vaibhavbairagi105@gmail.com>
2020-03-23 21:49:33 +0530

    UI and wiring done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BlockchainService } from 'src/app/services/blockchain.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CrazyCoinWebApp';
+  public blockchain;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.blockchain = blockchainService.blockchainInstance;
+  }
+
+  ngOnInit() {
+  }
+
+  thereArePendingTransactions() {
+    return this.blockchain.pendingTransactions.length > 0;
+  }
 }
